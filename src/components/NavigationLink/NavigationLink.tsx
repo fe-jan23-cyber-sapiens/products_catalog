69e4b08d188dffb2cca26e8046b3864c9081fa51
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-05-10 02:55:58 +0300

    adaptive header and styles utils were created

---
+++ src/components/NavigationLink/NavigationLink.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import { NavLink } from 'react-router-dom';
+
+import './NavigationLink.scss';
+
+type Props = {
+  to: string;
+  title: string;
+};
+
+export const NavigationLink: React.FC<Props> = ({ to, title }) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) => classNames(
+      'navbar__item',
+      { 'navbar__item--is-active': isActive },
+    )}
+  >
+    <span className="navbar__item-title">{title}</span>
+  </NavLink>
+);
