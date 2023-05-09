c81e7d346c15704f6d0211b8fa8260e02c0ebb69
Bogdan <bogdan_mykhailov@icloud.com>
2023-05-09 23:20:04 +0300

    :construction: remove template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import './main.scss';
-import { FC, useContext } from 'react';
-import { ThemeContext } from './context/ThemeContext';
+import { FC } from 'react';
 
 export const App: FC = () => {
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  const handleThemeChange = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-
   return (
-    <div className={theme}>
-      <div className="background">
-        <h2>hello cyber sapiens</h2>
-        <button type="button" onClick={handleThemeChange}>
-          Switch
-        </button>
-      </div>
-    </div>
+    <div />
   );
 };
