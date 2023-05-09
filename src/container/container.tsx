2b6214e81161d4377ef524847ff1739721360809
Denys Dubas <den.dubas.44@gmail.com>
2023-05-09 21:11:12 +0300

    Grid done

---
+++ src/container/container.tsx
@@ -0,0 +1,21 @@
+import './container.scss';
+
+export const App = () => (
+  <>
+    <div className="cards-container">
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+      <div className="container" />
+    </div>
+  </>
+);
