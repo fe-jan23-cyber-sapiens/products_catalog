8c52382dd3270a921009e83065e675fa958bb168
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-05-10 18:18:29 +0300

    test2

--- src/SliderItem/SliderItem.tsx
+++ src/SliderItem/SliderItem.tsx
@@ -1,17 +1,13 @@
 import { FC } from 'react';
 
 interface Props {
-  image: string,
+  image: string;
 }
 
 export const SliderItem: FC<Props> = ({ image }) => {
   return (
     <div className="carousel__image-box">
-      <img
-        src={image}
-        alt="Slider Phones"
-        className="carousel__image"
-      />
+      <img src={image} alt="Slider Phones" className="carousel__image" />
     </div>
   );
 };
--- src/components/SecondarySlider/SecondarySlider.tsx
+++ src/components/SecondarySlider/SecondarySlider.tsx
@@ -4,14 +4,11 @@ import * as images from '../../assets';
 import './SecondarySlider.scss';
 
 type Props = {
-  products: string[],
-  title: string,
+  products: string[];
+  title: string;
 };
 
-export const SecondarySlider: React.FC<Props> = ({
-  products,
-  title,
-}) => {
+export const SecondarySlider: React.FC<Props> = ({ products, title }) => {
   return (
     <section>
       <div className="secondary-slider__top-actions">
@@ -19,23 +16,17 @@ export const SecondarySlider: React.FC<Props> = ({
 
         <div className="secondary-slider__buttons">
           <div className="secondary-slider__button-left">
-            <img
-              src={images.arrow_left}
-              alt="Slider arrow to the left"
-            />
+            <img src={images.arrow_left} alt="Slider arrow to the left" />
           </div>
 
           <div className="secondary-slider__button-right">
-            <img
-              src={images.arrow_right}
-              alt="Slider arrow to the right"
-            />
+            <img src={images.arrow_right} alt="Slider arrow to the right" />
           </div>
         </div>
       </div>
-      {products.map(
-        product => <p className="secondary-slider__card">{product}</p>,
-      )}
+      {products.map((product) => (
+        <p className="secondary-slider__card">{product}</p>
+      ))}
     </section>
   );
 };
