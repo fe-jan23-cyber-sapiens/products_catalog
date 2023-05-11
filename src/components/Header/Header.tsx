64f19a6702b7bb849b0f92d5b9a9b5f6fd1d50ca
Bogdan <bogdan_mykhailov@icloud.com>
2023-05-11 16:51:53 +0300

    :construction: merge ThemeSwitcher branch

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import { NavBar } from '../NavBar';
 import './Header.scss';
 
 import * as images from '../../assets';
-import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
+import { ThemeSwitcher } from '../ThemeSwitcher';
 
 interface HeaderProps {
   onThemeChange: () => void;
