4bf3cbcee935b5fb0e5c37c71ffc9ce42d1539ca
Bogdan <bogdan_mykhailov@icloud.com>
2023-05-15 17:25:54 +0300

    :construction: refactor modal and card s size variable

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions,jsx-a11y/click-events-have-key-events */
 import { FC, ReactNode } from 'react';
 import './Modal.scss';
 import classNames from 'classnames';
@@ -14,16 +15,14 @@ export const Modal: FC<ModalProps> = ({
   closeModal,
 }) => {
   return (
-    <button
-      type="button"
+    <div
       className={classNames(
         'modal',
         { 'modal, modal--active': modalMode },
       )}
       onClick={closeModal}
     >
-      <button
-        type="button"
+      <div
         className={classNames(
           'modalContent',
           { 'modalContent, modalContent--active': modalMode },
@@ -31,7 +30,7 @@ export const Modal: FC<ModalProps> = ({
         onClick={event => event.stopPropagation()}
       >
         {children}
-      </button>
-    </button>
+      </div>
+    </div>
   );
 };
